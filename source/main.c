49ed6e56169b5b4f7d8b2d3e9c24b9e8b8bb9353
thedraculaisreal <jackjackson2546@gmail.com>
2024-11-29 23:43:46 +0000

    added raylib, and linked with lib

--- source/main.c
+++ source/main.c
@@ -3,10 +3,24 @@
 
 int main(void)
 {
-    struct entity ent = create_entity("vampie");
-    struct entity ent2 = create_entity("human");
-    print_entity(ent);
-    ent.health = take_damage(ent, ent2);
-    print_entity(ent);
+    InitWindow(1800, 900, "vampire-game");
+
+    while(!WindowShouldClose())
+    {
+	BeginDrawing();
+	ClearBackground(WHITE);
+	DrawText("First window", 400, 200, 12, RED);
+	EndDrawing();
+    }
+
+    CloseWindow();
+
     return 0;
 }
+
+
+/*struct entity ent = create_entity("vampie");
+struct entity ent2 = create_entity("human");
+print_entity(ent);
+ent.health = take_damage(ent, ent2);
+print_entity(ent);*/
